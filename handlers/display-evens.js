1e7d09720e84fd33da85263aad398ee39805cbaf
ayseakyol <ayseakyol.22309@gmail.com>
2020-03-10 23:31:32 +0100

    evens

--- handlers/display-evens.js
+++ handlers/display-evens.js
@@ -1,10 +1,19 @@
 function evensHandler() {
   debugger;
   // write me!
+  strings.displayed = "evens";
+  const currentDisplayed = strings.displayed;
+  const currentArray = strings[currentDisplayed];
+
+  const currentListComponent = renderList(currentArray, currentDisplayed);
+
+  const listContainer = document.getElementById("selected");
+  listContainer.innerHTML = "";
+  listContainer.appendChild(currentListComponent);
 
   // log user interaction: handler name, user input, new state
   log.push({
-    handler: 'evens',
+    handler: "evens",
     strings: JSON.parse(JSON.stringify(strings))
-  })
+  });
 }
